1d3a4dbeae2dd0bbd2f5873365f34dc426425a1f
- <=>
2020-11-17 06:08:13 +0700

    week1

---
+++ src/Ken.java
@@ -0,0 +1,16 @@
+public class Ken {
+    public static void main(String[] args) {
+        int i =  1;
+        float f = 3.0f;
+        double d = 3.0;
+        boolean b = true;
+        char c = 'c';
+        String s = "sos";
+
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(s);
+    }
+}
